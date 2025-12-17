dde95217bc80e3f8d7a45f9a88791d71836bbdde
sanand370 <srinidhianand28@gmail.com>
2025-12-16 17:01:32 -0800

    edits in OI and subsystem

--- src/main/java/org/tahomarobotics/robot/OI.java
+++ src/main/java/org/tahomarobotics/robot/OI.java
@@ -1,28 +1,12 @@
 package org.tahomarobotics.robot;
 
 
-import com.ctre.phoenix6.SignalLogger;
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.Pair;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import org.tinylog.Logger;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 
-import java.util.List;
-import java.util.Set;
-import java.util.function.Function;
-
-
-public class OI {
+public class OI{
 
 
     // -- Constants --
@@ -37,22 +21,19 @@ public class OI {
 
 
     private final CommandXboxController controller = new CommandXboxController(0);
+
     private final CommandXboxController lessImportantController = new CommandXboxController(1);
+    private final Elevator elevator;
 
 
-    public OI(RobotContainer robotContainer) {
-        DriverStation.silenceJoystickConnectionWarning(true);
 
 
+
+    public OI(RobotContainer elevator) {
+        this.elevator = new Elevator();
         configureControllerBindings();
-        configureLessImportantControllerBindings();
-
-
-        setDefaultCommands();
     }
 
-
-    // -- Bindings --
 
 
     public void configureControllerBindings() {
@@ -62,6 +43,8 @@ public class OI {
     controller.b().whileTrue(elevator.toggle());
     // make trigger for continuous and discrete
     }
+
+
 
 
     public void configureLessImportantControllerBindings() {
